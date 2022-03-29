a9241ec565b99f76c85828b24023bf9ff7f65177
Yahiaoui-Lydia <maitepaironi@gmail.com>
2022-03-29 18:49:49 +0100

    initialisation du projet

---
+++ db/db.js
@@ -0,0 +1,4 @@
+//Ce fichier exportera un module de connexions basé sur l'environnement
+const environment = process.env.NODE_ENV || 'development'
+const config = require('../knexfile')[environment]
+module.exports = require('knex')(config)
